2ef0cb6e28249c7816ab74af5b92c72526b26038
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-08-21 04:42:29 +0530

    Add files via upload

---
+++ 3_Implementation/main.c
@@ -0,0 +1,6 @@
+#include "test_encoder_decoder.h"
+int main(void)
+{
+	test_main();
+	return 0;
+}
---
+++ 3_Implementation/test/test_encoder_decoder.h
@@ -0,0 +1,6 @@
+#ifndef __TEST_ENCODER_DECODER_H__
+#define __TEST_ENCODER_DECODER_H__
+
+int test_main(void);
+
+#endif /* #ifndef __TEST_ENCODER_DECODER_H__ */
