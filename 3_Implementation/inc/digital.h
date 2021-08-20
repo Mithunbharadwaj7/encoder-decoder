27cc966af3664cd806e0fd4e1d82ce8c099ba32a
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-08-21 04:44:28 +0530

    Update digital.h

--- 3_Implementation/inc/digital.h
+++ 3_Implementation/inc/digital.h
@@ -1,14 +1,3 @@
-/**
- * @file conversion.h
- * @author Ambika C L
- * @brief Header files for temp,length,weight,numsystem and percentage function
- * @version 0.1
- * @date 2021-04-13
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-
 #ifndef __DIGITAL_H__
 #define __DIGITAL_H__
 
@@ -16,13 +5,6 @@
 #include<stdlib.h>
 int decoder(int n);
 
-/**
- * @brief Funtion for conversion of one number to another number system
- * 
- * @param[in] ch
- * @param[in] num1 
- * @return float result of number system conversion
- */
 int encoder(int n);
 
 #endif  /* __DIGITAL_H__  */
